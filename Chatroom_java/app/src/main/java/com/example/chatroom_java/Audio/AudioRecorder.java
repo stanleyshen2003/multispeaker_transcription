c72a38e4c8241935a9f841ad05b58a7ed382e8d6
yihsuan <yihsuanchen29@gmail.com>
2023-10-16 23:15:56 +0800

    i dont know why the record is not save

--- Chatroom_java/app/src/main/java/com/example/chatroom_java/Audio/AudioRecorder.java
+++ Chatroom_java/app/src/main/java/com/example/chatroom_java/Audio/AudioRecorder.java
@@ -7,6 +7,7 @@ import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.chatroom_java.Audio.IAudioCallback;
 
@@ -183,6 +184,7 @@ public class AudioRecorder {
                     FileUtils.clearFiles(filePaths);
                 } else {
                     //将多个pcm文件转化为wav文件
+                    Log.d("pcm","pcm");
                     pcmFilesToWavFile(filePaths);
                 }
             }
@@ -248,6 +250,7 @@ public class AudioRecorder {
      * 将音频信息写入文件
      */
     private void writeDataTOFile() {
+
         // new一个byte数组用来存一些字节数据，大小为缓冲区大小
         byte[] audioData = new byte[bufferSizeInBytes];
         FileOutputStream fos = null;
@@ -264,6 +267,7 @@ public class AudioRecorder {
                 file.delete();
             }
             // 建立一个可存取字节的文件
+
             fos = new FileOutputStream(file);
         } catch (IllegalStateException e) {
             e.printStackTrace();
