693de22bb8bb95cd024265bedc1f1fe111498480
KJLdefeated <linkai0508@gmail.com>
2023-10-16 12:33:12 +0800

    test

--- Chatroom_java/app/src/main/java/com/example/chatroom_java/MainActivity.java
+++ Chatroom_java/app/src/main/java/com/example/chatroom_java/MainActivity.java
@@ -1,14 +1,62 @@
-package com.example.chatroom_java;
+package com.example.chatroom_hackthon
 
-import androidx.appcompat.app.AppCompatActivity;
+import ChatAdapter
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatroom_hackthon.data.DataSource
+import com.example.chatroom_hackthon.data.loadJSONFromAsset
+import com.example.chatroom_hackthon.data.parseChatJSON
 
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ChatAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler_view)
+
+        val dataSource = DataSource.getDataSource(resources)
+        val chatList = dataSource.getChatList().value
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(this) // 设置LayoutManager为LinearLayoutManager
+
+        adapter = ChatAdapter(this, chatList ?: emptyList() ,recyclerView)
+        recyclerView.adapter = adapter
+
+        val recButton = findViewById<Button>(R.id.rec_button)
+        recButton.setOnClickListener {
+            //你要寫在這
+            //test
+
+
+            //----------------------------
+            val json = loadJSONFromAsset(baseContext, "chats.json")
+            val chatList = parseChatJSON(json)
+            val currentChatList = dataSource.getChatList().value?.toMutableList() ?: mutableListOf()
+            currentChatList.addAll(chatList)
+
+            dataSource.getChatList().postValue(currentChatList)
+            adapter.updateData(currentChatList)
+
+            recyclerView.postDelayed({
+                recyclerView.smoothScrollToPosition(currentChatList.size - 1)
+            }, 100)
+
+
+
+        }
+
+
+
     }
+
 }
+
