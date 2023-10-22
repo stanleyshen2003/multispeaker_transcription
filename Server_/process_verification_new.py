56f548a582a45bc017b2b60a755c8e9e1a6c0de2
stanleyshen2003 <stanley.shen2003@gmail.com>
2023-10-22 22:23:43 +0800

    tidying up

--- Server_/process_verification_new.py
+++ Server_/process_verification_new.py
@@ -1,28 +1,21 @@
-from speechbrain.pretrained import SepformerSeparation as separator
 from speechbrain.pretrained import SpeakerRecognition
 import torchaudio
-import os
-import numpy as np
-from hyperpyyaml import load_hyperpyyaml
 import json
-import torch
-from speechbrain.pretrained import EncoderDecoderASR
 import speech_recognition as sr
 import io 
+import google.generativeai as palm
 
-'''
-!!!! write your code under here !!!!
-'''
 class Voice_process_agent():
     '''
     init:
         separate_model_name: the model you want to use
         need_load: whether you have a local file of model
     '''
-    def __init__(self, verification_model_name = "spkrec-ecapa-voxceleb", need_load = True):
+    def __init__(self, verification_model_name = "spkrec-ecapa-voxceleb", need_load = True, apiKey = None):
         self.maxPeople = 5
         self.verification_model = self.load_verify_model(verification_model_name, need_load)
         self.r = sr.Recognizer()
+        palm.configure(api_key=apiKey)
 
         ''' 
         store all the audio files here
@@ -32,6 +25,7 @@ class Voice_process_agent():
         self.voice_record = []
         self.now_processing = None
         self.output_record = None
+        self.history = None
 
     def load_verify_model(self, model_name, need_load):
         """
@@ -52,6 +46,17 @@ class Voice_process_agent():
         '''
         waveform, _ = torchaudio.load(io.BytesIO(binary))
         return waveform
+
+    def language_model(self):
+        '''
+        if the two consecutive sentences belong to the same user
+        concat them and send it to the language model
+        '''
+        if self.history[1] == self.output_record[1] and self.output_record[1] == -1: 
+            response = palm.generate_text(prompt = "concat following two sentence with some modify to make it more reasonable and output only one sentence without any other message: " + self.output_record[0] + " , " + self.history[0])
+            result = response.result
+            self.output_record = [result, self.output_record[1]]
+
 
     def transcript(self, binary, who):
         '''
@@ -79,8 +84,7 @@ class Voice_process_agent():
         input: voice tensor * 2
         output: True/False
         '''
-        _, prediction = self.verification_model.verify_batch(voice1, voice2, threshold=0.3)
-        # print(prediction.item(), score)
+        _, prediction = self.verification_model.verify_batch(voice1, voice2, threshold=0.5)
         return prediction.item()
 
     def separate_user(self, data):
@@ -92,7 +96,6 @@ class Voice_process_agent():
         output: index of the person
         '''
         self.now_processing = self.bin_to_tensor(data)
-        # print(self.now_processing.shape)
         if len(self.voice_record) == 0:
             self.voice_record.append((self.now_processing, len(self.voice_record)+1))
             return 1
@@ -109,10 +112,16 @@ class Voice_process_agent():
         input: None (use self.output_record)
         output: JSON file
         '''
-        result_list = [{'text':self.output_record[0], 'name':'user'+str(self.output_record[1])}]
+        result_list = []
+        if self.history != None:
+            result_list = [{'text':self.history[0], 'name':'user'+str(self.history[1])}]
+        
+        result_list.append({'text':self.output_record[0], 'name':'user'+str(self.output_record[1])})
         result = json.dumps(result_list, indent=4)
-        with open('ouput.json', 'w') as output:
+        with open('output.json', 'w') as output:
             output.write(result)
+        if self.history != None:
+            self.output_record = [result_list[1]['text'], int(result_list[1]['name'][4:])]
         return result
 
     def process(self, data):
@@ -122,20 +131,21 @@ class Voice_process_agent():
         input: binary data
         output: JSON file
         '''
-        #data = open(data, "rb").read()
-        if len(self.voice_record) >= 5:
-            self.output_record = ["out of record", -1]
+        if self.output_record != None and self.output_record[0]!="###":
+            self.history = self.output_record
+        # data = open(data, "rb").read()
+
+        who = self.separate_user(data)
+        if who > self.maxPeople:
+            self.output_record = ['out of record, reset maximum record size if wanted', -1]
+            del self.voice_record[-1]
         else:
-            who = self.separate_user(data)
             self.transcript(data, who)
-        # print(who)
+        if self.history != None:
+            self.language_model()
         return self.to_json()
 
 
 if __name__ == "__main__":
-    agent = Voice_process_agent(need_load=True)
-    agent.process('backup/sound/grace.wav')
-    # agent.process('backup/sound/barren.wav')
-    agent.process('backup/sound/grace2.wav')
-    agent.process('backup/sound/barren.wav')
-    # print(len(agent.voice_record))
+    agent = Voice_process_agent(need_load=False)
+    agent.process(None)
